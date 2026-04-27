bf296eaad79b480ef73b0e492725dbd65733bc49
Anderson Brendon <andersonbrendonn@gmail.com>
2026-04-27 10:18:27 -0300

    feat(layout): add global react-hot-toast for notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
          <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div><Toaster/></div>
         {children}
       </body>
       </AppRouterCacheProvider>
